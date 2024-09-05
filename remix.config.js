7d6e0495b7f1a1b38af6442a81e19be610502480
iamtakagi <github@iamtakagi.net>
2024-09-05 11:57:34 +0900

    feat: tls for dev

---
+++ remix.config.js
@@ -0,0 +1,7 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+    dev: {
+        tlsKey: "tls/key.pem", // relative to cwd
+        tlsCert: "tls/cert.pem", // relative to cwd
+    },
+};
