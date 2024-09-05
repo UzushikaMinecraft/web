32c1327f8db9f478051a0634320324a3895b5c8a
iamtakagi <github@iamtakagi.net>
2024-09-06 07:07:06 +0900

    rm: remix.config.js

--- remix.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-    dev: {
-        tlsKey: "tls/key.pem", // relative to cwd
-        tlsCert: "tls/cert.pem", // relative to cwd
-    },
-};
